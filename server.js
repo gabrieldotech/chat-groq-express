7c034da2d71ae489070b15cb66d3ef4aefca5cc8
Milena Rodrigues <Milenarodrigues005@users.noreply.github.com>
2025-11-06 21:43:20 -0300

    feat: implementa middlewares e rotas principais do servidor E xpress

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 // Importa o controlador (Milena usa isso na sua implementação de rotas)
 const chatController = require("./controllers/chatController");
+const res = require("express/lib/response");
 
 // Usa dotenv para carregar GROQ_API_KEY do arquivo .env
 require("dotenv").config();
@@ -13,6 +14,14 @@ const port = 3000;
 // Configuração do EJS para as views
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", async (req, res) => {
+  const history = await chatController.loadHistory();
+  res.render("index", {history});
+});
+app.post("/chat", chatController.sendMessage);
 
 // =================================================================
 // TAREFA DA MILENA COMEÇA AQUI:
